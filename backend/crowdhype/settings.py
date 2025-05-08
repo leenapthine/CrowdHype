043ae456ae2cab3822061e605af8da23065bc389
Lee Napthine <153869055+leenapthine@users.noreply.github.com>
2025-05-07 21:21:12 -0700

    Update settings.py

--- backend/crowdhype/settings.py
+++ backend/crowdhype/settings.py
@@ -18,22 +18,15 @@ from environ import Env
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = Env()
+
 env_path = os.path.join(BASE_DIR, "crowdhype", ".env")
 if os.path.exists(env_path):
     env.read_env(env_path)
+
+SECRET_KEY = env('SECRET_KEY', default=os.environ.get('SECRET_KEY'))
+
 ENVIRONMENT = env('ENVIRONMENT', default='production')
 PORT = env.int("PORT", default=10000)
-
-# # Cloudflare R2 Configuration
-# AWS_ACCESS_KEY_ID = env("R2_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = env("R2_SECRET_ACCESS_KEY")
-# AWS_STORAGE_BUCKET_NAME = "crowdhype-videos"
-# AWS_S3_ENDPOINT_URL = "https://a29df9fc6efa87044ab737dc7167a69f.r2.cloudflarestorage.com"
-# AWS_S3_CUSTOM_DOMAIN = "pub-9facf8d7f0b64e0f85cdb56585205226.r2.dev"
-# AWS_QUERYSTRING_AUTH = False
-
-# # Use Cloudflare R2 for media storage
-# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STORAGES = {
     'default': {
@@ -47,9 +40,6 @@ STORAGES = {
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
 
 MEDIA_URL = '/media/'
 # MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
