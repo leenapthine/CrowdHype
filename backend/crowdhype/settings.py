2b20fd84a91485549eb6b4ebdbd796a1776b03ea
Lee Napthine <153869055+leenapthine@users.noreply.github.com>
2025-05-07 21:50:58 -0700

    Update settings.py

--- backend/crowdhype/settings.py
+++ backend/crowdhype/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+from django.core.exceptions import ImproperlyConfigured
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
